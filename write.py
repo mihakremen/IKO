58a6bd8c0f592d1eeea7429907a5380bace7fd57
mihakremen <mihakremen@gmail.com>
2022-05-18 11:27:24 +0100

    reformat for python3

--- write.py
+++ write.py
@@ -49,31 +49,31 @@ LINES_COL = (255, 255, 255)     # цвет линий
 ###########      определение  энкодеров      ###################
 
 def init():
-    GPIO.setwarnings(True)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(Enc_A1, GPIO.IN)
-    GPIO.setup(Enc_B1, GPIO.IN)
-    GPIO.setup(Enc_A2, GPIO.IN)
-    GPIO.setup(Enc_B2, GPIO.IN)
-    GPIO.setup(pwr_pin, GPIO.IN)
-    GPIO.add_event_detect(Enc_A1, GPIO.RISING, callback=rotation_decode_1) # замыкание ноги "а" 1 экнодера
-    GPIO.add_event_detect(Enc_B1, GPIO.RISING, callback=rotation_decode_1) # замыкание ноги "б" 1 экнодера
-    GPIO.add_event_detect(Enc_A2, GPIO.RISING, callback=rotation_decode_2) # замыкание ноги "а" 2 экнодера
-    GPIO.add_event_detect(Enc_B2, GPIO.RISING, callback=rotation_decode_2) # замыкание ноги "б" 2 экнодера
-    GPIO.add_event_detect(pwr_pin, GPIO.RISING, callback=turning_off)
-    return
+	GPIO.setwarnings(True)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(Enc_A1, GPIO.IN)
+	GPIO.setup(Enc_B1, GPIO.IN)
+	GPIO.setup(Enc_A2, GPIO.IN)
+	GPIO.setup(Enc_B2, GPIO.IN)
+	GPIO.setup(pwr_pin, GPIO.IN)
+	GPIO.add_event_detect(Enc_A1, GPIO.RISING, callback=rotation_decode_1) # замыкание ноги "а" 1 экнодера
+	GPIO.add_event_detect(Enc_B1, GPIO.RISING, callback=rotation_decode_1) # замыкание ноги "б" 1 экнодера
+	GPIO.add_event_detect(Enc_A2, GPIO.RISING, callback=rotation_decode_2) # замыкание ноги "а" 2 экнодера
+	GPIO.add_event_detect(Enc_B2, GPIO.RISING, callback=rotation_decode_2) # замыкание ноги "б" 2 экнодера
+	GPIO.add_event_detect(pwr_pin, GPIO.RISING, callback=turning_off)
+	return
 
 
 #####  обработка сигналов с 1 энкодера  #####
 def rotation_decode_1(A1_or_B1):
 	global angle, Rotary_counter, Current_A1, Current_B1, LockRotary
-    	sleep(0.002)
-    	Switch_A1 = GPIO.input(Enc_A1)
-    	Switch_B1 = GPIO.input(Enc_B1)
+	sleep(0.002)
+	Switch_A1 = GPIO.input(Enc_A1)
+	Switch_B1 = GPIO.input(Enc_B1)
 
 
 	if (Current_A1 == Switch_A1) and (Current_B1 == Switch_B1):		# Same interrupt as before (Bouncing)?
-    		return										# ignore interrupt!
+		return										# ignore interrupt!
 
 	Current_A1 = Switch_A1								# remember new state
 	Current_B1 = Switch_B1								# for next bouncing check
@@ -88,8 +88,8 @@ def rotation_decode_1(A1_or_B1):
 #     	      	        print "angle's change -> ", angle_send
 		else:					# Последней была замкнута нога А?
 			angle -= 0.03
-                        angle_send = int(angle*100)
-                        ser.write("a %d \n"%(angle_send))
+			angle_send = int(angle*100)
+			ser.write("a %d \n"%(angle_send))
 #                        print "angle's change -> ", angle_send
 		LockRotary.release()
 	return
@@ -99,45 +99,45 @@ def rotation_decode_1(A1_or_B1):
 #####     обработка сигналов со 2 энкодра   ####
 
 def rotation_decode_2(A2_or_B2):
-    	global marker, Rotary_counter, Current_A2, Current_B2, LockRotary
-    	sleep(0.002)
-    	Switch_A2 = GPIO.input(Enc_A2)
-    	Switch_B2 = GPIO.input(Enc_B2)
+	global marker, Rotary_counter, Current_A2, Current_B2, LockRotary
+	sleep(0.002)
+	Switch_A2 = GPIO.input(Enc_A2)
+	Switch_B2 = GPIO.input(Enc_B2)
 
 
-    	if (Current_A2 == Switch_A2) and (Current_B2 == Switch_B2):         # Same interrupt as before (Bouncing)?
+	if (Current_A2 == Switch_A2) and (Current_B2 == Switch_B2):         # Same interrupt as before (Bouncing)?
 		return                                                                          # ignore interrupt!
 
-    	Current_A2 = Switch_A2                                                         # remember new state
-    	Current_B2 = Switch_B2                                                          # for next bouncing check
+	Current_A2 = Switch_A2                                                         # remember new state
+	Current_B2 = Switch_B2                                                          # for next bouncing check
 
 
-    	if (Switch_A2 and Switch_B2):                                           # Both one active? Yes -> end of sequence
-        	LockRotary.acquire()                                            # get lock
-            	if A2_or_B2 == Enc_B2:                                                  # Turning direction depends on
-                	Rotary_counter += 0.05
+	if (Switch_A2 and Switch_B2):                                           # Both one active? Yes -> end of sequence
+		LockRotary.acquire()                                            # get lock
+		if A2_or_B2 == Enc_B2:                                                  # Turning direction depends on
+			Rotary_counter += 0.05
  #                   	marker_send = int(marker*100)
 #                    	ser.write("m %d \n"%(marker_send))
 #                    	print "marker pos -> ", marker_send                   # which input gave last in$
 			LockRotary.release()
 			sleep (0.3)
-            	else:                                                                           # so depending on direction either
-                    	Rotary_counter -= 0.05
+		else:                                                                           # so depending on direction either
+			Rotary_counter -= 0.05
 #		    	marker_send = int(marker*100)
 #                   	print "marker pos <- ", marker_send
 #		    	ser.write("m %d \n"%(marker_send))
 			LockRotary.release()
 			sleep (0.3)
-        return                                                                                  # THAT'S IT
+	return                                                                                  # THAT'S IT
 
 ##### функция выключения raspberry с кнопки "РУ"  №№№№№№№№№№№№№№№
 
 def turning_off(pwr_pin):
-        print 'System shuts down'
-        GPIO.cleanup()
+	print ('System shuts down')
+	GPIO.cleanup()
         #os.system("sudo reboot")
-        os.system("sudo shutdown -h now")
-        sleep(1)
+	os.system("sudo shutdown -h now")
+	sleep(1)
 
 
 init()
@@ -156,12 +156,12 @@ y_pos_north = screen_radius*math.sin(north_angle) # у координата ме
 ######    открытие порта serial     ########
 
 ser = serial.Serial(
-        port='/dev/ttyAMA0',            # указание порта
-        baudrate = 38400,               # скорость обмена данными
-        parity=serial.PARITY_NONE,      # равенство порта
-        stopbits=serial.STOPBITS_ONE,   # размерность байта
-        bytesize=serial.EIGHTBITS,      # число бит информации
-        timeout=1                       # ??? задержка в чтении порта
+	port='/dev/ttyAMA0',            # указание порта
+	baudrate = 38400,               # скорость обмена данными
+	parity=serial.PARITY_NONE,      # равенство порта
+	stopbits=serial.STOPBITS_ONE,   # размерность байта
+	bytesize=serial.EIGHTBITS,      # число бит информации
+	timeout=1                       # ??? задержка в чтении порта
 )
 
 #######################################################
@@ -169,21 +169,21 @@ ser = serial.Serial(
 #######################################################
 
 while True:
-    	sleep(0.06)
-    	clock.tick(FPS)
-    	LockRotary.acquire()                                    # get lock for rotary swit$
-   	NewCounter = Rotary_counter                     # get counter value
+	sleep(0.06)
+	clock.tick(FPS)
+	LockRotary.acquire()                                    # get lock for rotary swit$
+	NewCounter = Rotary_counter                     # get counter value
 	Rotary_counter = 0                                              # RESET IT TO 0
-    	LockRotary.release()
-    	if (NewCounter !=0):
+	LockRotary.release()
+	if (NewCounter !=0):
 		marker = marker + NewCounter
 		marker_send = int(marker*100)
-                ser.write("m %d \n"%(marker_send))
-                print "Direction  ", NewCounter, "marker pos ", marker_send                   # which input gave l$
+		ser.write("m %d \n"%(marker_send))
+	print ("Direction  ", NewCounter, "marker pos ", marker_send)                   # which input gave l$
 
-    	for event in pygame.event.get():
-       		if event.type == pygame.QUIT:       #закрытие окна
-        		raise SystemExit
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:       #закрытие окна
+			raise SystemExit
 #       	buttonIn = GPIO.input(26)
 #        if buttonIn == True:
 #        	print 'System shuts down'
@@ -194,47 +194,47 @@ while True:
 #        sleep(1)
     ######      расчет координат      ####
 
-    	x_pos_center = screen_radius*math.cos(angle) # х середины сектора
-    	y_pos_center = screen_radius*math.sin(angle) # y середины сектора
+	x_pos_center = screen_radius*math.cos(angle) # х середины сектора
+	y_pos_center = screen_radius*math.sin(angle) # y середины сектора
 
-    	x_pos_left = screen_radius*math.cos(angle+width) # х левого отрезка
-    	y_pos_left = screen_radius*math.sin(angle+width) # y левого отрезка
+	x_pos_left = screen_radius*math.cos(angle+width) # х левого отрезка
+	y_pos_left = screen_radius*math.sin(angle+width) # y левого отрезка
 
-    	x_pos_right = screen_radius*math.cos(angle-width) # х правого отрезка
-    	y_pos_right = screen_radius*math.sin(angle-width) # y правого отрезка
+	x_pos_right = screen_radius*math.cos(angle-width) # х правого отрезка
+	y_pos_right = screen_radius*math.sin(angle-width) # y правого отрезка
 
-    	x_pos_add = screen_radius*math.cos(marker) # х маркера
-    	y_pos_add = screen_radius*math.sin(marker) # y маркера
+	x_pos_add = screen_radius*math.cos(marker) # х маркера
+	y_pos_add = screen_radius*math.sin(marker) # y маркера
 
    ##########           отрисовка геометрии         #################
 
     #screen.fill(BACK_COL)           #снять коммент., если нужна заливка всего экрана
 
     # фон радара
-    	pygame.draw.circle(screen, (BACK_COL), (screen_center), screen_radius, 0)
+	pygame.draw.circle(screen, (BACK_COL), (screen_center), screen_radius, 0)
 
     # край радара
-    	pygame.draw.circle(screen, (LINES_COL), (screen_center), screen_radius, 2)
+	pygame.draw.circle(screen, (LINES_COL), (screen_center), screen_radius, 2)
 
     # компас
-    	pygame.draw.line(screen, (LINES_COL), (screen_center[0]+x_pos_north/1.15, screen_center[1]+y_pos_north/1.15), (screen_center[0]+x_pos_north, screen_center[1]+y_pos_north), 4)
+	pygame.draw.line(screen, (LINES_COL), (screen_center[0]+x_pos_north/1.15, screen_center[1]+y_pos_north/1.15), (screen_center[0]+x_pos_north, screen_center[1]+y_pos_north), 4)
 
     # центр. линия сектора
-    	pygame.draw.line(screen, (LINES_COL), (screen_center[0]+x_pos_center/2, screen_center[1]+y_pos_center/2), (screen_center[0]+x_pos_center, screen_center[1]+y_pos_center), 2)    #central line
+	pygame.draw.line(screen, (LINES_COL), (screen_center[0]+x_pos_center/2, screen_center[1]+y_pos_center/2), (screen_center[0]+x_pos_center, screen_center[1]+y_pos_center), 2)    #central line
 
     # левая линия сектора
-    	pygame.draw.line(screen, (LINES_COL), (screen_center[0], screen_center[1]), (screen_center[0]+x_pos_left, screen_center[1]+y_pos_left), 2)
+	pygame.draw.line(screen, (LINES_COL), (screen_center[0], screen_center[1]), (screen_center[0]+x_pos_left, screen_center[1]+y_pos_left), 2)
 
     # правая линия сектора
-    	pygame.draw.line(screen, (LINES_COL), (screen_center[0], screen_center[1]), (screen_center[0]+x_pos_right, screen_center[1]+y_pos_right), 2)
+	pygame.draw.line(screen, (LINES_COL), (screen_center[0], screen_center[1]), (screen_center[0]+x_pos_right, screen_center[1]+y_pos_right), 2)
 
     # маркер
-    	pygame.draw.line(screen, (LINES_COL), (screen_center[0]+x_pos_add/1.25, screen_center[1]+y_pos_add/1.25), (screen_center[0]+x_pos_add, screen_center[1]+y_pos_add), 4)
+	pygame.draw.line(screen, (LINES_COL), (screen_center[0]+x_pos_add/1.25, screen_center[1]+y_pos_add/1.25), (screen_center[0]+x_pos_add, screen_center[1]+y_pos_add), 4)
 
 
 ############        сихронизация положений элементов      #################
-    	if angle != angle_prev or width != width_prev or marker != marker_prev:
-        	pygame.display.update()
-        	angle_prev = angle
+	if angle != angle_prev or width != width_prev or marker != marker_prev:
+		pygame.display.update()
+		angle_prev = angle
 		width_prev = width
 		marker_prev = marker
